35aec66a1664f5dbcbc6fc876788e9daf832ec14
Ben Rexin <ben@rexin.at>
2015-07-14 19:20:05 +0200

    :art: group tests

--- test/controllers/v1/maps_controller_test.rb
+++ test/controllers/v1/maps_controller_test.rb
@@ -1,21 +1,25 @@
 require 'test_helper'
 
 class V1::MapsControllerTest < ActionController::TestCase
-  test "should list maps" do
-    get :index
+  class Index < self
+    test "should list maps" do
+      get :index
 
-    assert_response :success
+      assert_response :success
+    end
   end
 
-  test "should show one map" do
-    get :show, id: maps(:one).id
+  class Show < self
+    test "should show one map" do
+      get :show, id: maps(:one).id
 
-    assert_response :success
-  end
+      assert_response :success
+    end
 
-  test "should show 404 map not found" do
-    get :show, id: -1
+    test "should show 404 map not found" do
+      get :show, id: -1
 
-    assert_response :not_found
+      assert_response :not_found
+    end
   end
 end
