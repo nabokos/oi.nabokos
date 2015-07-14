34f8f426fee1554f611a96ca60ca0eb81899971f
Ben Rexin <ben@rexin.at>
2015-07-14 18:32:46 +0200

    :gift_heart: add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :nick,  presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :role,  presence: true
 end
